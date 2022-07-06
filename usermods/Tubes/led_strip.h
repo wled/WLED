e3f41449bfefd8da29ab8f448b5faa4d015c445c
Steve Eisner <steveeisner@gmail.com>
2022-07-06 00:03:15 -0700

    Light tubes are up and running

---
+++ usermods/Tubes/led_strip.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#define USE_WLED
+#include "wled.h"
+
+#define MAX_REAL_LEDS      64
+
+
+class LEDs {
+  public:
+    CRGB leds[MAX_REAL_LEDS];
+    // CRGB led_array[MAX_REAL_LEDS];
+
+    const static int FRAMES_PER_SECOND = 300;  // how often we refresh the strip, in frames per second
+    const static int REFRESH_PERIOD = 1000 / FRAMES_PER_SECOND;  // how often we refresh the strip, in milliseconds
+    int num_leds;
+
+    uint16_t fps = 0;
+
+  LEDs(int num_leds=MAX_REAL_LEDS) {
+    this->num_leds = num_leds;
+  }
+  
+  void setup() {
+    Serial.println((char *)F("LEDs: ok"));
+  }
+
+  void reverse() {
+    for (int i=1; i<8; i++) {
+      CRGB c = this->leds[i];
+      this->leds[i] = this->leds[16-i];
+      this->leds[16-i] = c;
+    }
+  }
+
+  void show() {
+    CRGB *external_buffer = WS2812FX::get_external_buffer();
+    for (int i = 0; i < num_leds; i++) {
+      external_buffer[i] = leds[i];
+    }
+  }
+  
+  void update(bool reverse=false) {
+    EVERY_N_MILLISECONDS( this->REFRESH_PERIOD ) {
+      // Update the LEDs
+      if (reverse)
+        this->reverse();
+      show();
+      this->fps++;
+    }
+
+    EVERY_N_MILLISECONDS( 1000 ) {
+      if (this->fps < (FRAMES_PER_SECOND - 30)) {
+        Serial.print(this->fps);
+        Serial.println((char *)F(" fps!"));
+      }
+      this->fps = 0;
+    }
+  }
+};
--- usermods/Tubes/virtual_strip.h
+++ usermods/Tubes/virtual_strip.h
@@ -168,21 +168,8 @@ class VirtualStrip {
     brightness = scale8(this->brightness, brightness);
 
     for (unsigned i=0; i < num_leds; i++) {
-#ifdef DOUBLED
-      uint8_t pos = (2*i + 1) % this->num_leds;  // slope of line is fixed right now at 2:1
-#else
       uint8_t pos = i;
-#endif
-
       CRGB c = this->leds[pos];
-
-#ifdef DOUBLED
-      CRGB c1 = this->leds[pos-1];
-      CRGB c2 = this->leds[pos+1];
-      nblend(c1, c, 128);
-      nblend(c, c2, 128);
-      nblend(c, c1, 128); // C is now a weighted average of the three virtual pixels
-#endif
 
       nscale8x3(c.r, c.g, c.b, brightness);
       nscale8x3(c.r, c.g, c.b, this->fader>>8);
