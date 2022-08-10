08757fd6381145d34b4595ff23cc9f65b9056751
Steve Eisner <steveeisner@gmail.com>
2022-08-10 13:41:36 -0400

    Start testing crossfading

--- usermods/Tubes/Tubes.h
+++ usermods/Tubes/Tubes.h
@@ -38,6 +38,12 @@ class TubesUsermod : public Usermod {
     void setup() {
       randomize();
 
+      // Override some behaviors on all Tubes
+      bootPreset = 0;  // Try to prevent initial playlists from starting
+      fadeTransition = true;  // Fade palette transitions
+      transitionDelay = 8000;   // Fade them for a long time
+      WS2812FX::load_pattern(DEFAULT_WLED_FX);  // Crossfade with FLOW
+
       // Start timing
       globalTimer.setup();
       beats.setup();
@@ -61,6 +67,31 @@ class TubesUsermod : public Usermod {
 
     void handleOverlayDraw()
     {
+      // Perform a cross-fade between current WLED mode and the external buffer
+
+      // uint8_t segment_id = strip.getMainSegmentId();
+      uint16_t length = strip.getLengthTotal();
+      auto external_buffer = strip.get_external_buffer();
+
+      uint8_t fade = sin8(millis() / 40); // amount that Tubes overwrites WLED, 0-255
+      
+      if (fade > 0) {
+        for (int i = 0, p = 0; i < length; i++, p++) {
+          if (p >= EXTERNAL_BUFFER_SIZE) {
+            p = 0;
+          }
+
+          CRGB color1 = strip.getPixelColor(i);
+          CRGB color2 = external_buffer[p];
+
+          uint8_t r = blend8(color1.r, color2.r, fade);
+          uint8_t g = blend8(color1.g, color2.g, fade);
+          uint8_t b = blend8(color1.b, color2.b, fade);
+
+          strip.setPixelColor(i, CRGB(r,g,b));
+        }
+      }
+
       // Draw effects layers over whatever WLED is doing.
       WS2812FX* leds = &strip;
       controller.effects->draw(leds);
