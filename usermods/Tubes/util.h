1554cb1fdfee834823c0a72954c5793cce94aa71
Steve Eisner <steveeisner@gmail.com>
2022-08-13 12:45:12 -0400

    Better debugging output

--- usermods/Tubes/util.h
+++ usermods/Tubes/util.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#include "wled.h"
-
-// Is this a tube that can control WLED?
-#define MASTER_TUBE
-
-#define DEFAULT_WLED_FX FX_MODE_EXPLODING_FIREWORKS
-
 uint8_t scaled16to8( uint16_t v, uint16_t lowest=0, uint16_t highest=65535) {
   uint16_t rangewidth = highest - lowest;
   uint16_t scaledbeat = scale16( v, rangewidth );
