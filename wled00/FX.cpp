1f14673f4392d83e16b5e5a8bca961986ad48067
Frank <frank.moehle@outlook.de>
2024-07-16 16:08:08 +0200

    workaround for "cannot read properties of undefined. reading 'split'
    hmpf ... looks like somewhere deep in WLED, the FX name string gets limited to 127 chars....

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -8547,7 +8547,7 @@ uint16_t mode_GEQLASER(void) {
   return FRAMETIME;
 
 }
-static const char _data_FX_MODE_GEQLASER[] PROGMEM = "GEQ 3D ☾@Speed,Front Fill,Horizon,Depth,Num Bands,Borders,Soft Light,;!,,Peaks;!;2f;sx=255,ix=255,c1=255,c2=255,c3=255,pal=11";
+static const char _data_FX_MODE_GEQLASER[] PROGMEM = "GEQ 3D ☾@Speed,Front Fill,Horizon,Depth,Num Bands,Borders,Soft,;!,,Peaks;!;2f;sx=255,ix=255,c1=255,c2=255,c3=255,pal=11";
 
 #endif // WLED_DISABLE_2D
 
