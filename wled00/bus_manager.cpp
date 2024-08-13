5610548652505e289a1dadbd547465f6a8aa712c
Frank <91616163+softhack007@users.noreply.github.com>
2024-08-13 12:52:19 +0200

    HUB75 clock phase
    `mxconfig.clkphase = false;` seems to help reduce flicker on some panels.
    
    @netmindz I think we should add a checkbox to the UI - something like "clocked on positive edge" (default = checked) which means clkphase = true. when unchecked, we set clkphase = false.

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -623,6 +623,7 @@ BusHub75Matrix::BusHub75Matrix(BusConfig &bc) : Bus(bc.type, bc.start, bc.autoWh
   // mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_10M;  // experimental - 5MHZ should be enugh, but colours looks slightly better at 10MHz
   //mxconfig.min_refresh_rate = 90;
   //mxconfig.min_refresh_rate = 120;
+  // mxconfig.clkphase = false;  // can help in case that the leftmost column is invisible, or pixels on the right side "bleeds out" to the left.
 
 #else
   USER_PRINTLN("MatrixPanel_I2S_DMA - Default pins");
