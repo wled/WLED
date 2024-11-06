cbd1ce470527645f853fe0afc14986296f39ec48
Frank <91616163+softhack007@users.noreply.github.com>
2024-11-07 00:06:46 +0100

    HUB75 hack-the-DOM
    * Reversed -> mxconfig.clkphase ; "Inverted clock phase"
    * Off Refresh -> mxconfig.latch_blanking = 1; "Single-cycle latch blanking"

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -593,8 +593,13 @@ BusHub75Matrix::BusHub75Matrix(BusConfig &bc) : Bus(bc.type, bc.start, bc.autoWh
   // mxconfig.latch_blanking = 3;                // use in case you see gost images
   // mxconfig.i2sspeed = HUB75_I2S_CFG::HZ_10M;  // experimental - 5MHZ should be enugh, but colours looks slightly better at 10MHz
   // mxconfig.min_refresh_rate = 90;
-  mxconfig.clkphase = false; // can help in case that the leftmost column is invisible, or pixels on the right side "bleeds out" to the left.
- 
+
+  mxconfig.clkphase = bc.reversed;
+  if (bc.refreshReq) mxconfig.latch_blanking = 1;                // needed for some ICS panels (default = 2)
+  // fake bus flags
+  _needsRefresh = mxconfig.latch_blanking == 1;
+  reversed = mxconfig.clkphase;
+
   // How many panels we have connected, cap at sane value
   mxconfig.chain_length = max((uint8_t) 1, min(bc.pins[0], (uint8_t) 4)); // prevent bad data preventing boot due to low memory
 
