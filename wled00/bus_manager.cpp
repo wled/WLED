9223b3c6cd95e6accf6fafe6c02fb66908a2fe33
Troy <5659019+troyhacks@users.noreply.github.com>
2024-11-10 13:34:30 -0500

    Free BusNetwork _data on cleanup

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -565,8 +565,8 @@ uint8_t BusNetwork::getPins(uint8_t* pinArray) const {
 void BusNetwork::cleanup() {
   _type = I_NONE;
   _valid = false;
-  // if (_data != nullptr) free(_data);
-  // _data = nullptr;
+  if (_data != nullptr) free(_data);
+  _data = nullptr;
 }
 
 // ***************************************************************************
