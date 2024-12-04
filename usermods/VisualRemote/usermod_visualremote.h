8f36f594c2b778f4f5d9c4ecb7363c3259ed5252
Pieter withaar <pieter.withaar@improveqs.nl>
2024-12-04 23:06:09 +0100

    trying to load and set menu patterns to json file

--- usermods/VisualRemote/usermod_visualremote.h
+++ usermods/VisualRemote/usermod_visualremote.h
@@ -107,135 +107,47 @@ inline void presetWithFallback_visualremote(uint8_t presetID, uint8_t effectID, 
   applyPresetWithFallback(presetID, CALL_MODE_BUTTON_PRESET, effectID, paletteID);
 }
 
+struct Pattern {
+  uint8_t id;           // ID of the pattern
+  String name;          // Name of the pattern
+  uint8_t length;       // Actual length of the pattern
+  uint8_t colors[12][4]; // Up to 12 RGB(W) colors
+};
+
+void serializePatternColors(JsonArray& json, uint8_t colors[12][4], uint8_t length) {
+  for (int i = 0; i < length; i++) {
+    JsonArray colorArray = json.createNestedArray();
+    colorArray.add(colors[i][0]);
+    colorArray.add(colors[i][1]);
+    colorArray.add(colors[i][2]);
+    colorArray.add(colors[i][3]);
+  }
+}
+
+void deserializePatternColors(JsonArray& json, uint8_t colors[12][4], uint8_t& length) {
+  length = json.size();
+  for (int i = 0; i < length; i++) {
+    JsonArray colorArray = json[i].as<JsonArray>();
+    if (!colorArray.isNull()) {
+      colors[i][0] = colorArray[0] | 0;
+      colors[i][1] = colorArray[1] | 0;
+      colors[i][2] = colorArray[2] | 0;
+      colors[i][3] = colorArray[3] | 0;
+    }
+  }
+}
+
 #define TOTAL_EFFECTS 20
 // Define a struct to hold the effect ID and its 5-pixel indicator pattern
-struct EffectIndicator {
-  uint8_t effectId;
-  uint32_t indicator[5]; // Colors for 5 pixels (in hex RGB format)
-};
-
-
-// Define the effectIndicators array for all 187 effects
-EffectIndicator effectIndicators[] = {
-  // Effect 0: FX_MODE_STATIC
-  {
-    FX_MODE_STATIC,
-    {0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF} // Solid white
-  },
-  // Effect 1: FX_MODE_BLINK
-  {
-    FX_MODE_BLINK,
-    {0xFFFFFF, 0x000000, 0xFFFFFF, 0x000000, 0xFFFFFF} // Alternating white and black
-  },
-  // Effect 2: FX_MODE_BREATH
-  {
-    FX_MODE_BREATH,
-    {0x666666, 0x999999, 0xCCCCCC, 0x999999, 0x666666} // Gradual brightness (gray shades)
-  },
-  // Effect 3: FX_MODE_COLOR_WIPE
-  {
-    FX_MODE_COLOR_WIPE,
-    {0xFF0000, 0x330000, 0x660000, 0x990000, 0xCC0000} // Shades of red wiping
-  },
-  // Effect 4: FX_MODE_COLOR_WIPE_RANDOM
-  {
-    FX_MODE_COLOR_WIPE_RANDOM,
-    {0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF} // Random colors
-  },
-  // Effect 5: FX_MODE_RANDOM_COLOR
-  {
-    FX_MODE_RANDOM_COLOR,
-    {0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF} // Random colors
-  },
-  // Effect 6: FX_MODE_COLOR_SWEEP
-  {
-    FX_MODE_COLOR_SWEEP,
-    {0x000000, 0x333333, 0x666666, 0x999999, 0xFFFFFF} // Sweeping from black to white
-  },
-  // Effect 7: FX_MODE_DYNAMIC
-  {
-    FX_MODE_DYNAMIC,
-    {0xFF0000, 0x00FF00, 0x0000FF, 0xFFFFFF, 0xFFFF00} // Dynamic mix of colors
-  },
-  // Effect 8: FX_MODE_RAINBOW
-  {
-    FX_MODE_RAINBOW,
-    {0xFF0000, 0xFF7F00, 0xFFFF00, 0x00FF00, 0x0000FF} // Red, Orange, Yellow, Green, Blue
-  },
-  // Effect 9: FX_MODE_RAINBOW_CYCLE
-  {
-    FX_MODE_RAINBOW_CYCLE,
-    {0x8B00FF, 0x4B0082, 0x0000FF, 0x00FF00, 0xFFFF00} // Violet, Indigo, Blue, Green, Yellow
-  },
-  // Effect 10: FX_MODE_SCAN
-  {
-    FX_MODE_SCAN,
-    {0x000000, 0x000000, 0xFF0000, 0x000000, 0x000000} // Single red pixel scanning
-  },
-  // Effect 11: FX_MODE_DUAL_SCAN
-  {
-    FX_MODE_DUAL_SCAN,
-    {0xFF0000, 0x000000, 0x000000, 0x000000, 0xFF0000} // Two red pixels at ends scanning
-  },
-  // Effect 12: FX_MODE_FADE
-  {
-    FX_MODE_FADE,
-    {0xFF0000, 0xFF4000, 0xFF8000, 0xFFBF00, 0xFFFF00} // Fading through colors
-  },
-  // Effect 13: FX_MODE_THEATER_CHASE
-  {
-    FX_MODE_THEATER_CHASE,
-    {0xFF0000, 0x000000, 0xFF0000, 0x000000, 0xFF0000} // Chasing red lights
-  },
-  // Effect 14: FX_MODE_THEATER_CHASE_RAINBOW
-  {
-    FX_MODE_THEATER_CHASE_RAINBOW,
-    {0xFF0000, 0xFF7F00, 0xFFFF00, 0x00FF00, 0x0000FF} // Chasing rainbow colors
-  },
-  // Effect 15: FX_MODE_RUNNING_LIGHTS
-  {
-    FX_MODE_RUNNING_LIGHTS,
-    {0xFFFFFF, 0x7F7F7F, 0x3F3F3F, 0x1F1F1F, 0x0F0F0F} // Running white lights fading
-  },
-  // Effect 16: FX_MODE_SAW
-  {
-    FX_MODE_SAW,
-    {0xFFFFFF, 0xCCCCCC, 0x999999, 0x666666, 0x333333} // Sawtooth effect with grays
-  },
-  // Effect 17: FX_MODE_TWINKLE
-  {
-    FX_MODE_TWINKLE,
-    {0xFFFFFF, 0x000000, 0xFFFFFF, 0x000000, 0xFFFFFF} // Twinkling white lights
-  },
-  // Effect 18: FX_MODE_DISSOLVE
-  {
-    FX_MODE_DISSOLVE,
-    {0xFF0000, 0xCC0000, 0x990000, 0x660000, 0x330000} // Dissolving reds
-  },
-  // Effect 19: FX_MODE_DISSOLVE_RANDOM
-  {
-    FX_MODE_DISSOLVE_RANDOM,
-    {0xFF69B4, 0x8A2BE2, 0x00CED1, 0x7FFF00, 0xFFD700} // Dissolving random colors
-  },
-  // Effect 20: FX_MODE_SPARKLE
-  {
-    FX_MODE_SPARKLE,
-    {0xFFFFFF, 0x000000, 0x000000, 0x000000, 0xFFFFFF} // Sparkle with white
-  }
-};
 
 uint8_t currentEffectIndex = 0;
 unsigned long lastTime = 0;
 bool isDisplayingEffectIndicator = false;     // Flag to indicate if indicator is currently displayed
 
-void displayEffectIndicator(uint8_t effectIndex) {
+void StartDisplayEffectIndicator(uint8_t effectIndex) {
     // Set the timestamp and flag
   lastTime = millis();
   isDisplayingEffectIndicator = true;
-  for (int i = 0; i < 5; i++) {
-    strip.setPixelColor(i, effectIndicators[effectIndex].indicator[i]);
-  }
-  strip.show();
 }
 
 void onButtonUpPress() {
@@ -243,7 +155,7 @@ void onButtonUpPress() {
   if (currentEffectIndex >= TOTAL_EFFECTS) {
     currentEffectIndex = 0;
   }
-  displayEffectIndicator(currentEffectIndex);
+  StartDisplayEffectIndicator(currentEffectIndex);
 }
 
 void onButtonDownPress() {
@@ -251,68 +163,8 @@ void onButtonDownPress() {
     currentEffectIndex = TOTAL_EFFECTS - 1;
   } else {
     currentEffectIndex--;
-  }
-  displayEffectIndicator(currentEffectIndex);
-}
-
-
-
-
-
-
-
-inline bool remoteJson_visualremote(int button) {
-  char objKey[10];
-  bool parsed = false;
-
-  if (!requestJSONBufferLock(22)) return false;
-
-  sprintf_P(objKey, PSTR("\"%d\":"), button);
-
-  readObjectFromFile(PSTR("/remote.json"), objKey, pDoc);
-  JsonObject fdo = pDoc->as<JsonObject>();
-  if (fdo.isNull()) {
-    releaseJSONBufferLock();
-    return parsed;
-  }
-
-  String cmdStr = fdo["cmd"].as<String>();
-  JsonObject jsonCmdObj = fdo["cmd"];
-
-  if (jsonCmdObj.isNull()) {
-    if (cmdStr.startsWith("!")) {
-      if (cmdStr.startsWith(F("!incBri"))) {
-        brightnessUp_visualremote();
-        parsed = true;
-      } else if (cmdStr.startsWith(F("!decBri"))) {
-        brightnessDown_visualremote();
-        parsed = true;
-      } else if (cmdStr.startsWith(F("!presetF"))) {
-        uint8_t p1 = fdo["PL"] | 1;
-        uint8_t p2 = fdo["FX"] | random8(strip.getModeCount() -1);
-        uint8_t p3 = fdo["FP"] | 0;
-        presetWithFallback_visualremote(p1, p2, p3);
-        parsed = true;
-      }
-    } else {
-      String apireq = "win"; apireq += '&';
-      if (!cmdStr.startsWith(apireq)) cmdStr = apireq + cmdStr;
-      if (!irApplyToAllSelected && cmdStr.indexOf(F("SS="))<0) {
-        char tmp[10];
-        sprintf_P(tmp, PSTR("&SS=%d"), strip.getMainSegmentId());
-        cmdStr += tmp;
-      }
-      fdo.clear();
-      handleSet(nullptr, cmdStr, false);
-      stateUpdated(CALL_MODE_BUTTON);
-      parsed = true;
-    }
-  } else {
-    deserializeState(jsonCmdObj, CALL_MODE_BUTTON);
-    parsed = true;
-  }
-  releaseJSONBufferLock();
-  return parsed;
+  }  
+  StartDisplayEffectIndicator(currentEffectIndex);
 }
 
 inline void handleRemote_visualremote(uint8_t *incomingData, size_t len) {
@@ -333,35 +185,110 @@ DEBUG_PRINT(cur_seq);
 DEBUG_PRINT(F("] button: "));
 DEBUG_PRINTLN(incoming->button);
 
-  if (!remoteJson_visualremote(incoming->button)) {
-    switch (incoming->button) {
-      case WIZMOTE_BUTTON_ON             : setOn_visualremote();                                         break;
-      case WIZMOTE_BUTTON_OFF            : setOff_visualremote();                                        break;
-      case WIZMOTE_BUTTON_ONE            : presetWithFallback_visualremote(1, FX_MODE_STATIC,        0); break;
-      case WIZMOTE_BUTTON_TWO            : presetWithFallback_visualremote(2, FX_MODE_BREATH,        0); break;
-      case WIZMOTE_BUTTON_THREE          : presetWithFallback_visualremote(3, FX_MODE_FIRE_FLICKER,  0); break;
-      case WIZMOTE_BUTTON_FOUR           : presetWithFallback_visualremote(4, FX_MODE_RAINBOW,       0); break;
-      case WIZMOTE_BUTTON_NIGHT          : activateNightMode_visualremote();                             break;
-      case WIZMOTE_BUTTON_BRIGHT_UP      : onButtonUpPress();                                            break;
-      case WIZMOTE_BUTTON_BRIGHT_DOWN    : onButtonDownPress();                                          break;
-      case WIZ_SMART_BUTTON_ON           : setOn_visualremote();                                         break;
-      case WIZ_SMART_BUTTON_OFF          : setOff_visualremote();                                        break;
-      case WIZ_SMART_BUTTON_BRIGHT_UP    : brightnessUp_visualremote();                                  break;
-      case WIZ_SMART_BUTTON_BRIGHT_DOWN  : brightnessDown_visualremote();                                break;
-      default: break;
-    }
+  switch (incoming->button) {
+    case WIZMOTE_BUTTON_ON             : setOn_visualremote();                                         break;
+    case WIZMOTE_BUTTON_OFF            : setOff_visualremote();                                        break;
+    case WIZMOTE_BUTTON_ONE            : presetWithFallback_visualremote(1, FX_MODE_STATIC,        0); break;
+    case WIZMOTE_BUTTON_TWO            : presetWithFallback_visualremote(2, FX_MODE_BREATH,        0); break;
+    case WIZMOTE_BUTTON_THREE          : presetWithFallback_visualremote(3, FX_MODE_FIRE_FLICKER,  0); break;
+    case WIZMOTE_BUTTON_FOUR           : presetWithFallback_visualremote(4, FX_MODE_RAINBOW,       0); break;
+    case WIZMOTE_BUTTON_NIGHT          : activateNightMode_visualremote();                             break;
+    case WIZMOTE_BUTTON_BRIGHT_UP      : onButtonUpPress();                                            break;
+    case WIZMOTE_BUTTON_BRIGHT_DOWN    : onButtonDownPress();                                          break;
+    case WIZ_SMART_BUTTON_ON           : setOn_visualremote();                                         break;
+    case WIZ_SMART_BUTTON_OFF          : setOff_visualremote();                                        break;
+    case WIZ_SMART_BUTTON_BRIGHT_UP    : brightnessUp_visualremote();                                  break;
+    case WIZ_SMART_BUTTON_BRIGHT_DOWN  : brightnessDown_visualremote();                                break;
+    default: break;
   }
+
   last_seq_visualremote = cur_seq;
 }
 
 // Usermod class
 class UsermodVisualRemote : public Usermod {
   private:
+   bool enabled = false;
+   // string that are used multiple time (this will save some flash memory)
+    static const char _name[];
+    static const char _enabled[];
     // Private members can be added here if needed
+    String presetNames[255]; 
+    bool preset_available[255];
+    Pattern patterns[255]; // Array to hold patterns for each preset
 
   public:
     void setup() {
       Serial.println("VisualRemote mod active!");
+      for (byte i = 0; i < 255; i++) {
+        preset_available[i] = getPresetName(i, presetNames[i]);        
+        if (preset_available[i]) {
+          Serial.printf("Preset %d: %s\n", i, presetNames[i].c_str());
+
+          // Initialize pattern data from config
+          if (patterns[i].name == "") {
+            // No pattern exists yet for this preset, create a default one
+            Serial.printf("No pattern for preset %d found, creating default pattern.\n", i);
+            patterns[i].id = i;
+            patterns[i].name = presetNames[i];
+            patterns[i].length = 3;
+            for (int j = 0; j < patterns[i].length; j++) {
+              patterns[i].colors[j][0] = 128; // Red
+              patterns[i].colors[j][1] = 128; // Green
+              patterns[i].colors[j][2] = 128; // Blue
+              patterns[i].colors[j][3] = 128; // White
+            }
+          } else {
+            Serial.printf("Pattern for preset %d loaded successfully.\n", i);
+          }
+        }
+      }
+    }
+
+    void addToConfig(JsonObject& root) override {
+      JsonObject top = root.createNestedObject(FPSTR(_name));
+      top[FPSTR(_enabled)] = enabled;
+
+      // Add patterns to config
+      JsonArray patternsArray = top.createNestedArray("patterns");
+      for (int i = 0; i < 255; i++) {
+        Serial.printf("Add pattern %d: %s\n", i, patterns[i].name.c_str());
+        if (patterns[i].name != "") {
+          JsonObject patternObj = patternsArray.createNestedObject();
+          patternObj["id"] = patterns[i].id;
+          patternObj["name"] = patterns[i].name;
+          patternObj["length"] = patterns[i].length;
+          JsonArray colorsArray = patternObj.createNestedArray("colors");
+          serializePatternColors(colorsArray, patterns[i].colors, patterns[i].length);
+        }
+      }
+    }
+
+    bool readFromConfig(JsonObject& root) override {
+      JsonObject top = root[FPSTR(_name)];
+
+      bool configComplete = !top.isNull();
+
+      configComplete &= getJsonValue(top[FPSTR(_enabled)], enabled);
+
+      // Read patterns from config
+      JsonArray patternsArray = top["patterns"].as<JsonArray>();
+      if (!patternsArray.isNull()) {
+        for (int i = 0; i < 255; i++) {
+          JsonObject patternObj = patternsArray[i];
+          if (!patternObj.isNull()) {
+            patterns[i].id = patternObj["id"] | 0;
+            patterns[i].name = patternObj["name"].as<String>();
+            patterns[i].length = patternObj["length"] | 0;
+            JsonArray colorsArray = patternObj["colors"].as<JsonArray>();
+            if (!colorsArray.isNull()) {
+              deserializePatternColors(colorsArray, patterns[i].colors, patterns[i].length);
+            }
+          }
+        }
+      }
+
+      return configComplete;
     }
 
     void loop() {
@@ -369,15 +296,15 @@ class UsermodVisualRemote : public Usermod {
     }
 
     bool onEspNowMessage(uint8_t* sender, uint8_t* data, uint8_t len) {
-      DEBUG_PRINT(F("CUSTOM ESP-NOW: "));
-      // You may need to comment out or define 'last_signal_src' appropriately
-      // DEBUG_PRINT(last_signal_src);
-      DEBUG_PRINT(F(" -> "));
-      DEBUG_PRINTLN(len);
-      for (int i = 0; i < len; i++) {
-        DEBUG_PRINTF_P(PSTR("%02x "), data[i]);
-      }
-      DEBUG_PRINTLN();
+      // DEBUG_PRINT(F("CUSTOM ESP-NOW: "));
+      // // You may need to comment out or define 'last_signal_src' appropriately
+      // // DEBUG_PRINT(last_signal_src);
+      // DEBUG_PRINT(F(" -> "));
+      // DEBUG_PRINTLN(len);
+      // for (int i = 0; i < len; i++) {
+      //   DEBUG_PRINTF_P(PSTR("%02x "), data[i]);
+      // }
+      // DEBUG_PRINTLN();
 
       // Handle Remote here
       handleRemote_visualremote(data, len);
@@ -389,17 +316,32 @@ class UsermodVisualRemote : public Usermod {
     void handleOverlayDraw()
     {
       if (isDisplayingEffectIndicator) {
-        for (int i = 0; i < 5; i++) {
-          strip.setPixelColor(i, effectIndicators[currentEffectIndex].indicator[i]);
+        int offset = 50; // Starting offset for the pattern
+        int patternLength = patterns[currentEffectIndex].length; // Dynamic length
 
+        for (int i = 0; i < patternLength; i++) {
+          int pixelIndex = offset + i;
+          uint8_t r = patterns[currentEffectIndex].colors[i][0];
+          uint8_t g = patterns[currentEffectIndex].colors[i][1];
+          uint8_t b = patterns[currentEffectIndex].colors[i][2];
+          uint8_t w = patterns[currentEffectIndex].colors[i][3];
+          strip.setPixelColor(i, RGBW32(r, g, b, w)); // Set the segment color
         }
+
         // Check if the indicator has been displayed for 5 seconds
-        if (millis() - lastTime > 1000) {
+        if (millis() - lastTime > 5000) {
           isDisplayingEffectIndicator = false;
-          effectCurrent = effectIndicators[currentEffectIndex].effectId;
-          colorUpdated(CALL_MODE_FX_CHANGED);
+          presetWithFallback_visualremote(patterns[currentEffectIndex].id, FX_MODE_STATIC,        0);
+
+          //effectCurrent = effectIndicators[currentEffectIndex].effectId;
+          //colorUpdated(CALL_MODE_FX_CHANGED);
         }
       }
-    }
+  };
 
 };
+
+
+// add more strings here to reduce flash memory usage
+const char UsermodVisualRemote::_name[]    PROGMEM = "VisualRemote";
+const char UsermodVisualRemote::_enabled[] PROGMEM = "enabled";
