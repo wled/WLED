4116785b44283e109d9b4ffb7e5f9255ee99903d
Frank <91616163+softhack007@users.noreply.github.com>
2024-12-30 22:55:03 +0100

    build 2412300

--- wled00/wled.h
+++ wled00/wled.h
@@ -7,7 +7,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2412200
+#define VERSION 2412300
 
 // WLEDMM  - you can check for this define in usermods, to only enabled WLEDMM specific code in the "right" fork. Its not defined in AC WLED.
 #define _MoonModules_WLED_
