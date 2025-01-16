63db2c9fc11a35214439d3194e2c585c82cc09ae
Frank <91616163+softhack007@users.noreply.github.com>
2025-01-16 13:07:43 +0100

    prepare pinmager for -P4
    support up to 56 GPIO pins (0 thru 55)

--- wled00/pin_manager.h
+++ wled00/pin_manager.h
@@ -75,8 +75,8 @@ class PinManagerClass {
   PinOwner ownerTag[WLED_NUM_PINS] = { PinOwner::None };
   PinOwner ownerConflict[WLED_NUM_PINS] = { PinOwner::None }; // WLEDMM: record pin alloc conflicts
   #else
-  #define WLED_NUM_PINS 50
-  uint8_t pinAlloc[7] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //WLEDMM bugfix - 56bit, 1 bit per pin, we use 50 bits on -S3
+  #define WLED_NUM_PINS 56 // S3 has 49 gpio, P4 has 55 gpio
+  uint8_t pinAlloc[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //WLEDMM bugfix - 64bit, 1 bit per pin, we use 50 bits on -S3, 55 on -P4
   uint8_t ledcAlloc[2] = {0x00, 0x00}; //16 LEDC channels
   PinOwner ownerTag[WLED_NUM_PINS] = { PinOwner::None }; // new MCU's have up to 50 GPIO
   PinOwner ownerConflict[WLED_NUM_PINS] = { PinOwner::None }; // WLEDMM: record pin alloc conflicts
