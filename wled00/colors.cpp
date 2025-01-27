2306a66a1d7e7c8a3de2207946184bafb9fd2539
Blaž Kristan <blaz@kristan-sp.si>
2025-01-27 21:26:35 +0100

    Rework color_from_palette() - respect paletteBlend - bugfix in ColorFromPaletteWLED()

--- wled00/colors.cpp
+++ wled00/colors.cpp
@@ -92,21 +92,25 @@ uint32_t ColorFromPaletteWLED(const CRGBPalette16& pal, unsigned index, uint8_t 
   if (blendType == LINEARBLEND_NOWRAP) {
     index = (index*240) >> 8; // Blend range is affected by lo4 blend of values, remap to avoid wrapping
   }
-  unsigned hi4 = byte(index) >> 4;
-  const CRGB* entry = (CRGB*)((uint8_t*)(&(pal[0])) + (hi4 * sizeof(CRGB)));
+  unsigned hi4 = (index & 0xF0) >> 4;
+  unsigned lo4 = (index & 0x0F);
+  const CRGB* entry = (CRGB*)&(pal[0]) + hi4;
+  //const CRGB* entry = (CRGB*)((uint8_t*)(&(pal[0])) + (hi4 * sizeof(CRGB)));
   unsigned red1   = entry->r;
   unsigned green1 = entry->g;
   unsigned blue1  = entry->b;
-  if (blendType != NOBLEND) {
+  if (lo4 && blendType != NOBLEND) {
     if (hi4 == 15) entry = &(pal[0]);
     else ++entry;
-    unsigned f2 = ((index & 0x0F) << 4) + 1; // +1 so we scale by 256 as a max value, then result can just be shifted by 8
+    unsigned f2 = (lo4 << 4) + 1; // +1 so we scale by 256 as a max value, then result can just be shifted by 8
     unsigned f1 = (257 - f2); // f2 is 1 minimum, so this is 256 max
-    red1   = (red1 * f1 + (unsigned)entry->r * f2) >> 8;
-    green1 = (green1 * f1 + (unsigned)entry->g * f2) >> 8;
-    blue1  = (blue1 * f1 + (unsigned)entry->b * f2) >> 8;
+    // actually color_blend(c1, c2, lo4<<4);
+    red1   = (red1   * f1 + entry->r * f2) >> 8;
+    green1 = (green1 * f1 + entry->g * f2) >> 8;
+    blue1  = (blue1  * f1 + entry->b * f2) >> 8;
   }
   if (brightness < 255) { // note: zero checking could be done to return black but that is hardly ever used so it is omitted
+    // actually color_fade(c1, brightness)
     uint32_t scale = brightness + 1; // adjust for rounding (bitshift)
     red1   = (red1 * scale) >> 8;
     green1 = (green1 * scale) >> 8;
