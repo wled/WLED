a5f77645e598ecac4b5c9423400f30d596445de3
cleverark <james.finnegan@clevercraft.pro>
2025-03-23 21:31:47 -0500

    feat: Early version sync

---
+++ variants/xiao_esp32s3/pins_arduino.h
@@ -0,0 +1,30 @@
+#ifndef _VARIANT_XIAO_ESP32S3_
+#define _VARIANT_XIAO_ESP32S3_
+
+// Onboard LED
+#define LED_BUILTIN 21
+
+// I2C
+#define PIN_WIRE_SDA 6
+#define PIN_WIRE_SCL 7
+
+// SPI
+#define PIN_SPI_MISO 10
+#define PIN_SPI_MOSI 11
+#define PIN_SPI_SCK  9
+#define PIN_SPI_SS   8
+
+// UART
+#define PIN_SERIAL_RX 44
+#define PIN_SERIAL_TX 43
+
+// Analog Pins
+#define A0 1
+#define A1 2
+#define A2 3
+#define A3 4
+#define A4 5
+#define A5 6
+#define A6 7
+
+#endif
