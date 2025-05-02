f69455883023e6df6fa5b70ac552f1f6006e4522
rhkean <rhkean@gmail.com>
2025-05-02 12:58:58 -0400

    msg

--- usermods/BLE/BLE.cpp
+++ usermods/BLE/BLE.cpp
@@ -1,112 +1,37 @@
 #include "BLE.h"
+#include "NimBLEDevice.h"
 
-/**  None of these are required as they will be handled by the library with defaults. **
- **                       Remove as you see fit for your needs                        */
+/** Handler class for server events */
 class ServerCallbacks : public NimBLEServerCallbacks {
     void onConnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo) override {
-        printf("Client address: %s\n", connInfo.getAddress().toString().c_str());
-
-        /**
-         *  We can use the connection handle here to ask for different connection parameters.
-         *  Args: connection handle, min connection interval, max connection interval
-         *  latency, supervision timeout.
-         *  Units; Min/Max Intervals: 1.25 millisecond increments.
-         *  Latency: number of intervals allowed to skip.
-         *  Timeout: 10 millisecond increments.
-         */
-        pServer->updateConnParams(connInfo.getConnHandle(), 24, 48, 0, 180);
+        printf("Client connected:: %s\n", connInfo.getAddress().toString().c_str());
     }
 
     void onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason) override {
-        printf("Client disconnected - start advertising\n");
-        NimBLEDevice::startAdvertising();
-    }
-
-    void onMTUChange(uint16_t MTU, NimBLEConnInfo& connInfo) override {
-        printf("MTU updated: %u for connection ID: %u\n", MTU, connInfo.getConnHandle());
-    }
-
-    /********************* Security handled here *********************/
-    uint32_t onPassKeyDisplay() override {
-        printf("Server Passkey Display\n");
-        /**
-         * This should return a random 6 digit number for security
-         *  or make your own static passkey as done here.
-         */
-        return 123456;
-    }
-
-    void onConfirmPassKey(NimBLEConnInfo& connInfo, uint32_t pass_key) override {
-        printf("The passkey YES/NO number: %" PRIu32 "\n", pass_key);
-        /** Inject false if passkeys don't match. */
-        NimBLEDevice::injectConfirmPasskey(connInfo, true);
-    }
-
-    void onAuthenticationComplete(NimBLEConnInfo& connInfo) override {
-        /** Check that encryption was successful, if not we disconnect the client */
-        if (!connInfo.isEncrypted()) {
-            NimBLEDevice::getServer()->disconnect(connInfo.getConnHandle());
-            printf("Encrypt connection failed - disconnecting client\n");
-            return;
-        }
-
-        printf("Secured connection to: %s\n", connInfo.getAddress().toString().c_str());
+        printf("Client disconnected - sleeping for %" PRIu32 " seconds\n", sleepSeconds);
+        esp_deep_sleep_start();
     }
 } serverCallbacks;
 
-/** Handler class for characteristic actions */
-class CharacteristicCallbacks : public NimBLECharacteristicCallbacks {
-    void onRead(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) override {
-        printf("%s : onRead(), value: %s\n",
-               pCharacteristic->getUUID().toString().c_str(),
-               pCharacteristic->getValue().c_str());
-    }
-
-    void onWrite(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) override {
-        printf("%s : onWrite(), value: %s\n",
-               pCharacteristic->getUUID().toString().c_str(),
-               pCharacteristic->getValue().c_str());
-    }
-
-    /**
-     *  The value returned in code is the NimBLE host return code.
-     */
-    void onStatus(NimBLECharacteristic* pCharacteristic, int code) override {
-        printf("Notification/Indication return code: %d, %s\n", code, NimBLEUtils::returnCodeToString(code));
-    }
-
-    /** Peer subscribed to notifications/indications */
-    void onSubscribe(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo, uint16_t subValue) override {
-        std::string str  = "Client ID: ";
-        str             += connInfo.getConnHandle();
-        str             += " Address: ";
-        str             += connInfo.getAddress().toString();
-        if (subValue == 0) {
-            str += " Unsubscribed to ";
-        } else if (subValue == 1) {
-            str += " Subscribed to notifications for ";
-        } else if (subValue == 2) {
-            str += " Subscribed to indications for ";
-        } else if (subValue == 3) {
-            str += " Subscribed to notifications and indications for ";
+/** Callback class to handle advertising events */
+class AdvertisingCallbacks : public NimBLEExtAdvertisingCallbacks {
+    void onStopped(NimBLEExtAdvertising* pAdv, int reason, uint8_t instId) override {
+        /* Check the reason advertising stopped, don't sleep if client is connecting */
+        printf("Advertising instance %u stopped\n", instId);
+        switch (reason) {
+            case 0:
+                printf("Client connecting\n");
+                return;
+            case BLE_HS_ETIMEOUT:
+                printf("Time expired - sleeping for %" PRIu32 " seconds\n", sleepSeconds);
+                break;
+            default:
+                break;
         }
-        str += std::string(pCharacteristic->getUUID());
 
-        printf("%s\n", str.c_str());
+        esp_deep_sleep_start();
     }
-} characteristicCallbacks;
-
-/** Handler class for descriptor actions */
-class DescriptorCallbacks : public NimBLEDescriptorCallbacks {
-    void onWrite(NimBLEDescriptor* pDescriptor, NimBLEConnInfo& connInfo) override {
-        std::string dscVal = pDescriptor->getValue();
-        printf("Descriptor written value: %s\n", dscVal.c_str());
-    }
-
-    void onRead(NimBLEDescriptor* pDescriptor, NimBLEConnInfo& connInfo) override {
-        printf("%s Descriptor read\n", pDescriptor->getUUID().toString().c_str());
-    }
-} descriptorCallbacks;
+} advertisingCallbacks;
 
 class BLEUsermod : public Usermod {
 
@@ -128,185 +53,79 @@ class BLEUsermod : public Usermod {
 
     void setup()
     {
-        /*
-        if(enabled)
-        {
-            DEBUG_PRINTLN(F("Initializing BLE..."));
-            um_data = new um_data_t();
-            um_data->u_size = 1;
-            um_data->u_type = new um_types_t[um_data->u_size];
-            um_data->u_data = new void*[um_data->u_size];
-            um_data->u_data[0] = &deviceConnected;      //*used (New)
-            um_data->u_type[0] = UMT_BYTE;
-
-            DEBUG_PRINTLN(F("Calling ini..."));
-            NimBLEDevice::init(serverDescription);
-
-            // Create the BLE Server
-            DEBUG_PRINTLN(F("creating server..."));
-            _pServer = NimBLEDevice::createServer();
-            DEBUG_PRINTLN(F("setting callbacks..."));
-            _pServer->setCallbacks(new MyServerCallbacks());
-            DEBUG_PRINTLN(F("disabe advertise on disconnect..."));
-            _pServer->advertiseOnDisconnect(false);
-
-            // Create the BLE Service
-            DEBUG_PRINTLN(F("Creating service..."));
-            _pService = _pServer->createService(SERVICE_UUID);
-
-            // Create a BLE Characteristic
-            DEBUG_PRINTLN(F("creating characteristic..."));
-            _pCharacteristic = _pService->createCharacteristic( CHARACTERISTIC_UUID,
-                                                                NIMBLE_PROPERTY::READ
-                                                              | NIMBLE_PROPERTY::WRITE
-                                                              | NIMBLE_PROPERTY::NOTIFY );
-
-            // Start the service
-            DEBUG_PRINTLN(F("starting service..."));
-            _pService->start();
-
-            // Start advertising
-            DEBUG_PRINTLN(F("get advertising..."));
-            _pAdvertising = NimBLEDevice::getAdvertising();
-            _pAdvertising->addServiceUUID(SERVICE_UUID);
-            _pAdvertising->enableScanResponse(false);
-            _pAdvertising->setAdvertisingCompleteCallback(onAdvertisingComplete);
-            DEBUG_PRINTLN(F("starting advertising..."));
-            _pAdvertising->start();
-            DEBUG_PRINTLN(F("Waiting a client connection to notify..."));
-        }
-        */
-
-        printf("Starting NimBLE Server\n");
-
         /** Initialize NimBLE and set the device name */
-        NimBLEDevice::init("NimBLE");
+        NimBLEDevice::init("Extended advertiser");
 
-        /**
-        * Set the IO capabilities of the device, each option will trigger a different pairing method.
-        *  BLE_HS_IO_DISPLAY_ONLY    - Passkey pairing
-        *  BLE_HS_IO_DISPLAY_YESNO   - Numeric comparison pairing
-        *  BLE_HS_IO_NO_INPUT_OUTPUT - DEFAULT setting - just works pairing
-        */
-        // NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_ONLY); // use passkey
-        // NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_YESNO); //use numeric comparison
-
-        /**
-        *  2 different ways to set security - both calls achieve the same result.
-        *  no bonding, no man in the middle protection, BLE secure connections.
-        *
-        *  These are the default values, only shown here for demonstration.
-        */
-        // NimBLEDevice::setSecurityAuth(false, false, true);
-
-        NimBLEDevice::setSecurityAuth(/*BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
-        pServer = NimBLEDevice::createServer();
+        /** Create the server and add the services/characteristics/descriptors */
+        NimBLEServer* pServer = NimBLEDevice::createServer();
         pServer->setCallbacks(&serverCallbacks);
 
-        NimBLEService*        pDeadService = pServer->createService("DEAD");
-        NimBLECharacteristic* pBeefCharacteristic =
-            pDeadService->createCharacteristic("BEEF",
-                                                NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE |
-                                                    /** Require a secure connection for read and write access */
-                                                    NIMBLE_PROPERTY::READ_ENC | // only allow reading if paired / encrypted
-                                                    NIMBLE_PROPERTY::WRITE_ENC  // only allow writing if paired / encrypted
-            );
+        NimBLEService*        pService = pServer->createService(SERVICE_UUID);
+        NimBLECharacteristic* pCharacteristic =
+            pService->createCharacteristic(CHARACTERISTIC_UUID,
+                                        NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY);
 
-        pBeefCharacteristic->setValue("Burger");
-        pBeefCharacteristic->setCallbacks(&characteristicCallbacks);
+        pCharacteristic->setValue("Hello World");
+
+        /** Start the service */
+        pService->start();
 
         /**
-        *  2902 and 2904 descriptors are a special case, when createDescriptor is called with
-        *  either of those uuid's it will create the associated class with the correct properties
-        *  and sizes. However we must cast the returned reference to the correct type as the method
-        *  only returns a pointer to the base NimBLEDescriptor class.
-        */
-        NimBLE2904* pBeef2904 = pBeefCharacteristic->create2904();
-        pBeef2904->setFormat(NimBLE2904::FORMAT_UTF8);
-        pBeef2904->setCallbacks(&descriptorCallbacks);
+         *  Create an extended advertisement with the instance ID 0 and set the PHY's.
+         *  Multiple instances can be added as long as the instance ID is incremented.
+         */
+        NimBLEExtAdvertisement extAdv(primaryPhy, secondaryPhy);
 
-        NimBLEService*        pBaadService = pServer->createService("BAAD");
-        NimBLECharacteristic* pFoodCharacteristic =
-            pBaadService->createCharacteristic("F00D", NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY);
+        /** Set the advertisement as connectable */
+        extAdv.setConnectable(true);
 
-        pFoodCharacteristic->setValue("Fries");
-        pFoodCharacteristic->setCallbacks(&characteristicCallbacks);
+        /** As per Bluetooth specification, extended advertising cannot be both scannable and connectable */
+        extAdv.setScannable(false); // The default is false, set here for demonstration.
 
-        /** Custom descriptor: Arguments are UUID, Properties, max length of the value in bytes */
-        NimBLEDescriptor* pC01Ddsc =
-            pFoodCharacteristic->createDescriptor("C01D",
-                                                    NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_ENC,
-                                                    20);
-        pC01Ddsc->setValue("Send it back!");
-        pC01Ddsc->setCallbacks(&descriptorCallbacks);
+        /** Extended advertising allows for 251 bytes (minus header bytes ~20) in a single advertisement or up to 1650 if chained */
+        extAdv.setServiceData(NimBLEUUID(SERVICE_UUID),
+                            std::string("Extended Advertising Demo.\r\n"
+                                        "Extended advertising allows for "
+                                        "251 bytes of data in a single advertisement,\r\n"
+                                        "or up to 1650 bytes with chaining.\r\n"
+                                        "This example message is 226 bytes long "
+                                        "and is using CODED_PHY for long range."));
 
-        /** Start the services when finished creating all Characteristics and Descriptors */
-        pDeadService->start();
-        pBaadService->start();
+        extAdv.setName("Extended advertiser");
 
-        /** Create an advertising instance and add the services to the advertised data */
-        NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
-        pAdvertising->setName("NimBLE-Server");
-        pAdvertising->addServiceUUID(pDeadService->getUUID());
-        pAdvertising->addServiceUUID(pBaadService->getUUID());
+        /** When extended advertising is enabled `NimBLEDevice::getAdvertising` returns a pointer to `NimBLEExtAdvertising */
+        NimBLEExtAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+
+        /** Set the callbacks for advertising events */
+        pAdvertising->setCallbacks(&advertisingCallbacks);
+
         /**
-        *  If your device is battery powered you may consider setting scan response
-        *  to false as it will extend battery life at the expense of less data sent.
-        */
-        pAdvertising->enableScanResponse(true);
-        pAdvertising->start();
+         *  NimBLEExtAdvertising::setInstanceData takes the instance ID and
+         *  a reference to a `NimBLEExtAdvertisement` object. This sets the data
+         *  that will be advertised for this instance ID, returns true if successful.
+         *
+         *  Note: It is safe to create the advertisement as a local variable if setInstanceData
+         *  is called before exiting the code block as the data will be copied.
+         */
+        if (pAdvertising->setInstanceData(0, extAdv)) {
+            /**
+             *  NimBLEExtAdvertising::start takes the advertisement instance ID to start
+             *  and a duration in milliseconds or a max number of advertisements to send (or both).
+             */
+            if (pAdvertising->start(0, advTime)) {
+                printf("Started advertising\n");
+            } else {
+                printf("Failed to start advertising\n");
+            }
+        } else {
+            printf("Failed to register advertisement data\n");
+        }
 
-        printf("Advertising Started\n");
-
-        initDone = true;
+        esp_sleep_enable_timer_wakeup(sleepSeconds * 1000000);
     }
   
     void loop()
     {
-        /*
-        // notify changed value
-        if (deviceConnected) {
-        pCharacteristic->setValue((uint8_t*)&value, 4);
-        pCharacteristic->notify();
-        value++;
-        }
-
-        if (!deviceConnected && oldDeviceConnected) {
-            pAdvertising = NimBLEDevice::getAdvertising();
-            if (NimBLEDevice::getWhiteListCount() > 0) {
-                // Allow anyone to scan but only whitelisted can connect.
-                pAdvertising->setScanFilter(false,true);
-            }
-            // advertise with whitelist for 30 seconds
-            pAdvertising->start(30 * 1000);
-            DEBUG_PRINTLN(F("start advertising"));
-            oldDeviceConnected = deviceConnected;
-        }
-
-        if (deviceConnected && !oldDeviceConnected) {
-            // do stuff here on connecting
-            oldDeviceConnected = deviceConnected;
-        }
-        */
-        if (pServer->getConnectedCount())
-        {
-            NimBLEService* pSvc = pServer->getServiceByUUID("BAAD");
-            if (pSvc)
-            {
-                NimBLECharacteristic* pChr = pSvc->getCharacteristic("F00D");
-                if (pChr)
-                {
-                    pChr->notify();
-                }
-            }
-        }
-    }
-
-    bool getUMData(um_data_t **data) override
-    {
-        if (!data || !_enabled) return false; // no pointer provided by caller or not enabled -> exit
-            *data = um_data;
-        return true;
     }
 
     bool readFromConfig(JsonObject& root) override
--- usermods/BLE/BLE.h
+++ usermods/BLE/BLE.h
@@ -1,4 +1,6 @@
-
+#ifndef USERMOD_BLE
+    #define USERMOD_BLE
+#endif
 #define CONFIG_BT_ENABLED                   y
 #define CONFIG_BTDM_CTRL_MODE_BLE_ONLY      y
 #define CONFIG_BTDM_CTRL_MODE_BR_EDR_ONLY   n
@@ -15,5 +17,20 @@
 // https://www.uuidgenerator.net/
 #define SERVICE_UUID        "9b01c315-9649-4360-9b3c-2599f6f28d65"
 #define CHARACTERISTIC_UUID "220f62a4-b861-40d4-bffa-91e911ace3a4"
+
+/** Time in milliseconds to advertise */
+static uint32_t advTime = 5000;
+
+/** Time to sleep between advertisements */
+static uint32_t sleepSeconds = 20;
+
+/** Primary PHY used for advertising, can be one of BLE_HCI_LE_PHY_1M or BLE_HCI_LE_PHY_CODED */
+static uint8_t primaryPhy = BLE_HCI_LE_PHY_1M;
+
+/**
+ *  Secondary PHY used for advertising and connecting,
+ *  can be one of BLE_HCI_LE_PHY_1M, BLE_HCI_LE_PHY_2M or BLE_HCI_LE_PHY_CODED
+ */
+static uint8_t secondaryPhy = BLE_HCI_LE_PHY_2M;
 
 
