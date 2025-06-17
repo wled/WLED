64f4d14fc6937a0605ef71156e46a72e76a9d30c
Will Tatam <will@netmindz.net>
2025-06-17 19:44:39 +0100

    Add Particle System playlist

--- wled00/data/index.js
+++ wled00/data/index.js
@@ -3375,6 +3375,7 @@ function genPresets()
 				}
 				addToPlaylist("All", ef.id, "ALL");
 				if(ef.name.startsWith("Y💡")) addToPlaylist("AnimARTrix", ef.id, "AM");
+				if(ef.name.startsWith("PS ")) addToPlaylist("Particle System", ef.id, "PS");
 				if (m.includes("1")) addToPlaylist("All 1D", ef.id, "1D");
 				if (m.includes("2")) addToPlaylist("All 2D", ef.id, "2D");
 
