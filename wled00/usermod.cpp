4ae3e849272a9e4ff4f406cd676aa5f7d912e91c
MarcelMuxio <marcel@muxio.nl>
2025-07-24 15:37:26 +0200

    Update usermod.cpp

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -2,7 +2,7 @@
 #include "../usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h"
 
 void registerUsermods() {
-  usermods.add(new Usermod_RotaryRackDimmer());
+  userMods.add(new Usermod_RotaryRackDimmer());
 }
 
 /*
