e7601f99c259964e60150bd931087805670092ed
MarcelMuxio <marcel@muxio.nl>
2025-07-24 14:36:48 +0200

    Update usermod_rotaryrackdimmer.h

--- usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
+++ usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h
@@ -1,5 +1,18 @@
-#include "usermods/usermod_rotaryrackdimmer/usermod_rotaryrackdimmer.h"
+#pragma once
+#include "wled.h"
 
-void registerUsermods() {
-  usermods.add(new Usermod_RotaryRackDimmer());
-}
+class Usermod_RotaryRackDimmer : public Usermod {
+  public:
+    void setup() override {
+      Serial.println("RotaryRackDimmer setup klaar!");
+    }
+
+    void loop() override {
+      // Simuleer helderheid verhogen (test!)
+      if (millis() % 5000 < 50) {
+        bri = (bri + 10) % 255;
+        applyBri();
+        Serial.printf("Helderheid aangepast naar: %d\n", bri);
+      }
+    }
+};
