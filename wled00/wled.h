4b42f6bbe29a03807ea7c694dd474c2aaec3d560
Will Tatam <will@netmindz.net>
2025-07-26 19:54:09 +0100

    update to 0.15.1-rc1

--- wled00/wled.h
+++ wled00/wled.h
@@ -7,7 +7,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2502220
+#define VERSION 2507260
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
