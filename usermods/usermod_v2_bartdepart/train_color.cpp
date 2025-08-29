353d70894540aab66dc675bda353108e291ef082
Ken Sedgwick <ken@bonsai.com>
2025-08-29 13:03:37 -0700

    bartdepart: Guard against null and accept case-insensitive input

--- usermods/usermod_v2_bartdepart/train_color.cpp
+++ usermods/usermod_v2_bartdepart/train_color.cpp
@@ -1,13 +1,15 @@
 #include <cstring>
+#include <strings.h> // for strcasecmp
 
 #include "train_color.h"
 
 TrainColor parseTrainColor(const char * name) {
-  if      (strcmp(name, "RED")==0)    return TrainColor::Red;
-  else if (strcmp(name, "ORANGE")==0) return TrainColor::Orange;
-  else if (strcmp(name, "YELLOW")==0) return TrainColor::Yellow;
-  else if (strcmp(name, "GREEN")==0)  return TrainColor::Green;
-  else if (strcmp(name, "BLUE")==0)   return TrainColor::Blue;
-  else if (strcmp(name, "WHITE")==0)  return TrainColor::White;
+  if (!name) return TrainColor::Unknown;
+  if      (strcasecmp(name, "RED")==0)    return TrainColor::Red;
+  else if (strcasecmp(name, "ORANGE")==0) return TrainColor::Orange;
+  else if (strcasecmp(name, "YELLOW")==0) return TrainColor::Yellow;
+  else if (strcasecmp(name, "GREEN")==0)  return TrainColor::Green;
+  else if (strcasecmp(name, "BLUE")==0)   return TrainColor::Blue;
+  else if (strcasecmp(name, "WHITE")==0)  return TrainColor::White;
   return TrainColor::Unknown;
 }
