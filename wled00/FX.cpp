9f8dbf3091d3dd350830fdc62416897ab9363012
Damian Schneider <daedae@gmx.ch>
2025-09-13 08:42:01 +0200

    improved integer overflows, added static mode

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -4884,11 +4884,11 @@ uint16_t mode_shimmer() {
   if(!SEGENV.allocateData(sizeof(uint32_t))) { return mode_static(); }
   uint32_t* lastTime = reinterpret_cast<uint32_t*>(SEGENV.data);
 
-  uint32_t radius = (SEGMENT.custom1 * SEGLEN >> 7) + 1;     // [1, 2*SEGLEN+1] pixels
-  uint32_t traversalDistance = (SEGLEN + 2 * radius) << 8;   // total subpixels to cross, 1 pixel = 256 subpixels
-  uint32_t traversalTime = 200 + (255 - SEGMENT.speed) * 80; // [200, 20600] ms
-  uint32_t speed = (((traversalDistance << 5) / traversalTime) << 5);  // subpixels/"second" note: total shift is 10 to approximate seconds, split shifts to guarantee no overflows
-  int32_t  position = static_cast<int32_t>(SEGENV.step);     // current position in subpixels
+  uint32_t radius = (SEGMENT.custom1 * SEGLEN >> 7) + 1;        // [1, 2*SEGLEN+1] pixels
+  uint32_t traversalDistance = (SEGLEN + 2 * radius) << 8;      // total subpixels to cross, 1 pixel = 256 subpixels
+  uint32_t traversalTime = 200 + (255 - SEGMENT.speed) * 80;    // [200, 20600] ms
+  uint32_t speed = ((traversalDistance << 5) / traversalTime);  // subpixels/512ms
+  int32_t  position = static_cast<int32_t>(SEGENV.step);        // current position in subpixels
   uint16_t inputstate = (uint16_t(SEGMENT.intensity) << 8) | uint16_t(SEGMENT.custom1); // current user input state
 
   // init
@@ -4899,32 +4899,36 @@ uint16_t mode_shimmer() {
     SEGENV.aux1 = inputstate; // save user input state
   }
 
-  uint32_t deltaTime = strip.now - *lastTime;
-  *lastTime = strip.now;
+  if(SEGMENT.speed) {
+    uint32_t deltaTime = (strip.now - *lastTime) & 0x7F; // clamp to 127ms to avoid overflows. note: speed*deltaTime can still overflow for segments > ~10k pixels
+    *lastTime = strip.now;
 
-  if (SEGENV.aux0 > 0) {
-    SEGENV.aux0 = (SEGENV.aux0 > deltaTime) ? SEGENV.aux0 - deltaTime : 0;
-  } else {
-    // calculate movement step and update position
-    int32_t step = 1 + ((speed * deltaTime) >> 10); // subpixels moved this frame. note >>10 approximates ~substeps/seconds
-    position += step;
-    int endposition = (SEGLEN + radius) << 8;
-    if (position > endposition) {
-      SEGENV.aux0 = SEGMENT.intensity * 236; // [0, 60180] ms pause
-      if(SEGMENT.check3) SEGENV.aux0 = hw_random(SEGENV.aux0 + 1000); // randomise interval, +1 second to affect low intensity values
-      position = -(radius << 8); // reset to start position (out of frame)
+    if (SEGENV.aux0 > 0) {
+      SEGENV.aux0 = (SEGENV.aux0 > deltaTime) ? SEGENV.aux0 - deltaTime : 0;
+    } else {
+      // calculate movement step and update position
+      int32_t step = 1 + ((speed * deltaTime) >> 5); // subpixels moved this frame. note >>5 as speed is in subpixels/512ms
+      position += step;
+      int endposition = (SEGLEN + radius) << 8;
+      if (position > endposition) {
+        SEGENV.aux0 = SEGMENT.intensity * 236; // [0, 60180] ms pause
+        if(SEGMENT.check3) SEGENV.aux0 = hw_random(SEGENV.aux0 + 1000); // randomise interval, +1 second to affect low intensity values
+        position = -(radius << 8); // reset to start position (out of frame)
+      }
+      SEGENV.step = (uint32_t)position; // save back
     }
-    SEGENV.step = (uint32_t)position; // save back
-  }
 
-  if (SEGMENT.check2)
-    position = (SEGLEN << 8) - position;   // invert position (and direction)
+    if (SEGMENT.check2)
+      position = (SEGLEN << 8) - position;   // invert position (and direction)
+  } else {
+    position = (SEGLEN << 7); // at speed=0, make it static in the center (this enables to use modulators only)
+  }
 
   for (int i = 0; i < SEGLEN; i++) {
     uint32_t dist = abs(position - (i << 8));
-    if (dist <= (radius << 8)) {
+    if (dist < (radius << 8)) {
       uint32_t color = SEGMENT.color_from_palette(i * 255 / SEGLEN, false, false, 0);
-      uint8_t blend = (dist * 255) / (radius << 8); // linear gradient
+      uint8_t blend = dist / radius; // linear gradient note: dist is in subpixels, radius in pixels, result is [0, 255] since dist < radius*256
       if (SEGMENT.custom2) {
         uint8_t modVal; // modulation value
         if (SEGMENT.check1) {
