de01c5e61f7ae292fc3c6f5e29f2c8f3a837816d
Aogu181 <83169487+Aogu181@users.noreply.github.com>
2025-12-03 09:15:32 +0800

    Add Gledopto Series With Ethernet

--- wled00/network.cpp
+++ wled00/network.cpp
@@ -145,6 +145,8 @@ const ethernet_settings ethernetBoards[] = {
     ETH_PHY_LAN8720,      // eth_type,
     ETH_CLOCK_GPIO0_OUT	// eth_clk_mode
   },
+
+ // Gledopto Series With Ethernet
  {
     1,                    // eth_address, 
     5,                    // eth_power, 
