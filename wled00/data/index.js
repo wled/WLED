c35140e763e6354d1a97410313c295c10ce72e8b
Damian Schneider <daedae@gmx.ch>
2025-12-13 23:49:53 +0100

    "WLEDPixelForge": new image & scrolling text interface (#4982)
    replaces the pixel magic tool with a much more feature-rich tool for handling gif images. Also adds a scrolling text interface and the possibility to add more tools with a single button click like the classic pixel magic tool and the pixel-painter tool.

--- wled00/data/index.js
+++ wled00/data/index.js
@@ -1276,7 +1276,6 @@ function updateUI()
 	gId('buttonPower').className = (isOn) ? 'active':'';
 	gId('buttonNl').className = (nlA) ? 'active':'';
 	gId('buttonSync').className = (syncSend) ? 'active':'';
-	gId('pxmb').style.display = (isM) ? "inline-block" : "none";
 
 	updateSelectedFx();
 	updateSelectedPalette(selectedPal); // must be after updateSelectedFx() to un-hide color slots for * palettes
