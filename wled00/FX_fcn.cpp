8b6a57180e7f80ece1606acaaaddbf8d47c078f8
Damian Schneider <daedae@gmx.ch>
2025-12-16 22:38:41 +0100

    set unused segments to black (#5192)
    0.15.1 introduced a new behaviour on how "empty space" is treated. while it is technically correct, it is not user friendly and is a breaking change: all previous versions behaved differently and so does 0.16.

--- wled00/FX_fcn.cpp
+++ wled00/FX_fcn.cpp
@@ -466,7 +466,7 @@ void Segment::setGeometry(uint16_t i1, uint16_t i2, uint8_t grp, uint8_t spc, ui
   #endif
 
   m12 = constrain(m12, 0, 7);
-  if (stop && (spc > 0 || m12 != map1D2D)) fill(BLACK);
+  if (stop) fill(BLACK); // turn off LEDs in segment before changing geometry
   if (m12 != map1D2D) map1D2D = m12;
 /*
   if (boundsUnchanged
