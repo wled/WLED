c9f47d4b5c309fcbc6e913c8d0d28e1eecf0d57a
Frank Möhle <91616163+softhack007@users.noreply.github.com>
2026-01-28 23:37:22 +0100

    new ESP32 node types
    Added new node types for unsupported ESP32 variants, based on same file from ESP Easy.
    
    Just to be prepared for new nodes (future support)

--- wled00/NodeStruct.h
+++ wled00/NodeStruct.h
@@ -15,6 +15,22 @@
 #define NODE_TYPE_ID_ESP32S3         34
 #define NODE_TYPE_ID_ESP32C3         35
 
+// updated node types from the ESP Easy project
+// https://github.com/letscontrolit/ESPEasy/blob/mega/src/src/DataTypes/NodeTypeID.h
+//#define NODE_TYPE_ID_ESP32        33
+//#define NODE_TYPE_ID_ESP32S2      34
+//#define NODE_TYPE_ID_ESP32C3      35
+//#define NODE_TYPE_ID_ESP32S3      36
+#define NODE_TYPE_ID_ESP32C2      37
+#define NODE_TYPE_ID_ESP32H2      38
+#define NODE_TYPE_ID_ESP32C6      39
+#define NODE_TYPE_ID_ESP32C61     40
+#define NODE_TYPE_ID_ESP32C5      41
+#define NODE_TYPE_ID_ESP32P4      42
+#define NODE_TYPE_ID_ESP32P4r3    45
+#define NODE_TYPE_ID_ESP32H21     43
+#define NODE_TYPE_ID_ESP32H4      44
+
 /*********************************************************************************************\
 * NodeStruct
 \*********************************************************************************************/
