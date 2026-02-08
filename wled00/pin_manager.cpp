9232bd4d762f57cd71d239afb3f5cfbe40cb5edd
Frank <91616163+softhack007@users.noreply.github.com>
2026-02-08 20:26:10 +0100

    correct straping pin comments in pinManager

--- wled00/pin_manager.cpp
+++ wled00/pin_manager.cpp
@@ -233,14 +233,14 @@ bool PinManager::isPinOk(byte gpio, bool output)
     // JTAG: GPIO39-42 are usually used for inline debugging
     // GPIO46 is input only and pulled down
   #elif defined(CONFIG_IDF_TARGET_ESP32C5)
-    // strapping pins: 2, 7, 27, 28
+    // strapping pins: (2), 7, 25, 26, 27, 28 (GPIO2 is not a strapping pin; it's used only for external JTAG when GPIO7 selects it)
     // GPIO 0-15 directly usable, 16-22 are for SPI flash
     if (gpio > 15 && gpio < 23) return false;     // 16-22 SPI FLASH
     #if ARDUINO_USB_CDC_ON_BOOT == 1 || ARDUINO_USB_DFU_ON_BOOT == 1
     if (gpio == 13 || gpio == 14) return false;   // 13-14 USB-JTAG
     #endif
   #elif defined(CONFIG_IDF_TARGET_ESP32C6)
-    // strapping pins: 8, 9, 15
+    // strapping pins: 4, 5, 8, 9, 15  (GPIO4/MTMS and GPIO5/MTDI are also strapping pins)
     // GPIO 0-23 directly usable, 24-30 are for SPI flash
     if (gpio > 23 && gpio < 31) return false;     // 24-30 SPI FLASH
     #if ARDUINO_USB_CDC_ON_BOOT == 1 || ARDUINO_USB_DFU_ON_BOOT == 1
