92fb0ebdff5596c5230d5c9eb0f44ce0d6b17914
GLEDOPTO <aogu@gledopto.com>
2026-03-02 13:44:35 +0800

    feat(eps-now): add 3 scenes for GLEDOPTO ESP-NOW WLED Remote
    This PR adds three new scenes to the EPS-NOW remote controller to support the GLEDOPTO ESP-NOW WLED Remote Control.
    
    Product link: https://gledopto.com/h-pd-146.html

--- wled00/remote.cpp
+++ wled00/remote.cpp
@@ -220,7 +220,7 @@ void handleRemote() {
       case WIZMOTE_BUTTON_TWO            : presetWithFallback(2, FX_MODE_BREATH,        0); break;
       case WIZMOTE_BUTTON_THREE          : presetWithFallback(3, FX_MODE_FIRE_FLICKER,  0); break;
       case WIZMOTE_BUTTON_FOUR           : presetWithFallback(4, FX_MODE_RAINBOW,       0); break;
-      case WIZMOTE_BUTTON_FIVE           : presetWithFallback(5, FX_MODE_SCAN,          0); break;
+      case WIZMOTE_BUTTON_FIVE           : presetWithFallback(5, FX_MODE_FIRE_FLICKER,  0); break;
       case WIZMOTE_BUTTON_SIX            : presetWithFallback(6, FX_MODE_BLINK,         0); break;
       case WIZMOTE_BUTTON_SEVEN          : presetWithFallback(7, FX_MODE_FADE,          0); break;
       case WIZMOTE_BUTTON_NIGHT          : activateNightMode();                             break;
