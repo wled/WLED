446b2cd2ccc7f996b0397c08f36ed8637eaf1611
Damian Schneider <daedae@gmx.ch>
2026-04-29 20:07:19 +0200

    another check for malformed packet

--- wled00/e131.cpp
+++ wled00/e131.cpp
@@ -118,9 +118,9 @@ void handleE131Packet(e131_packet_t* p, IPAddress clientIP, byte protocol, size_
     if (packetLen < 126) return; // need up to property_values[0] (offset 125) and property_value_count (offset 123)
     // Ignore PREVIEW data (E1.31: 6.2.6)
     if ((p->options & 0x80) != 0) return;
-    dmxChannels = htons(p->property_value_count) - 1;
+    dmxChannels = htons(p->property_value_count) - 1; // on malformed packets, this can become negative, checked below
     // DMX level data is zero start code. Ignore everything else. (E1.11: 8.5)
-    if (dmxChannels == 0 || p->property_values[0] != 0) return;
+    if (dmxChannels <= 0 || p->property_values[0] != 0) return;
     uni = htons(p->universe);
     e131_data = p->property_values;
     seq = p->sequence_number;
