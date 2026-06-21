219c4c1eb23ec106bb085ca92085cba11c3c861c
Frank <91616163+softhack007@users.noreply.github.com>
2026-06-21 21:47:52 +0200

    Fix unreachable ESP32-S3 reboot-warning branch.

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -1134,9 +1134,8 @@ void BusHub75Matrix::cleanup() {
   if (display != nullptr) delete display;
   display = nullptr;
   virtualDisp = nullptr; // note: when not using "NO_GFX" this causes a memory leak
-  #if defined(CONFIG_IDF_TARGET_ESP32S3)  // runtime reconfiguration is not working on -S3
+  #else  // runtime reconfiguration is not working on -S3, request reboot from user instead
     errorFlag = ERR_REBOOT_NEEDED;
-  #endif
   #endif
   if (_ledBuffer != nullptr) d_free(_ledBuffer); _ledBuffer = nullptr;
   if (_ledsDirty != nullptr) d_free(_ledsDirty); _ledsDirty = nullptr;
