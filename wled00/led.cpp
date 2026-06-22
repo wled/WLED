0b0fb88908ce9c20329c61c46a7822032692debb
Damian Schneider <daedae@gmx.ch>
2026-06-22 09:43:21 +0200

    hacky fix also for color fade

--- wled00/led.cpp
+++ wled00/led.cpp
@@ -231,8 +231,10 @@ void handleNightlight() {
       {
         for (unsigned i=0; i<4; i++) colPri[i] = colNlT[i]+ ((colSec[i] - colNlT[i])*nper);   // fading from actual color to secondary color
       }
+      uint16_t transitionduration = strip.getTransition();
+      strip.setTransition(0); // temporary disable transition and set color & brightness directly, (fix for #5620)
       colorUpdated(CALL_MODE_NO_NOTIFY);
-      applyFinalBri(); // colorUpdated() re-starts transition (if enabled) every 100ms, preventing brightness from changing correctly (fix for #5620)
+      strip.setTransition(transitionduration); // restore transition time to previous value. Note: this is a dirty hack and needs proper fix by disabling transitions completely in nightlight mode
     }
     if (nper >= 1) //nightlight duration over
     {
