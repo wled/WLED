7d04b3744a89cdba693cbea50e40e4d932d05e63
yazdan-iot <yazdan.electrical.eng@gmail.com>
2026-07-11 13:17:03 +0330

    refactor: split apClients underflow check into two lines for readability

--- wled00/network.cpp
+++ wled00/network.cpp
@@ -421,7 +421,8 @@ void WiFiEvent(WiFiEvent_t event)
   switch (event) {
     case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED:
       // AP client disconnected
-      if (apClients > 0 && --apClients == 0 && isWiFiConfigured()) forceReconnect = true; // no clients reconnect WiFi if available
+      if (apClients > 0) apClients--;
+      if (apClients == 0 && isWiFiConfigured()) forceReconnect = true; // no clients reconnect WiFi if available
       DEBUG_PRINTF_P(PSTR("WiFi-E: AP Client Disconnected (%d) @ %lus.\n"), (int)apClients, millis()/1000);
       break;
     case ARDUINO_EVENT_WIFI_AP_STACONNECTED:
