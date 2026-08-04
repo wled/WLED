87ad1f1737fca65105e64427ed79f8f1ae617e06
Damian Schneider <daedae@gmx.ch>
2026-08-04 20:34:55 +0200

    lots of improvements after testing

--- lib/wled_ADCmanager/wled_ADCmanager.cpp
+++ lib/wled_ADCmanager/wled_ADCmanager.cpp
@@ -1,10 +1,23 @@
 /*
  * ADC manager to handle continous ADC sampling in parallel with single-shot pin reads
  * by @dedehai (2026) licensed under EUPL 1.2 license
+ *
+ * calling begin() will start sampling an ADC pin in continuous mode
+ * any subsequent call for analogRead() or analogReadMilliVolts() will stop the continuous sampling immediately,
+ * drain the already sampled data into a buffer, read a pin in one-shot mode, then continue the sampling.
+ * replaces analogRead() and analogReadMilliVolts() with managed functions for code compatibility
+
+ TODO:
+ - could add the option to use hardware IIR filter, although the lowest coefficient setting of 2 already has a 3dB cutoff around 2kHz (to be confirmed) at 20kHz sample rate
+ - IIR filter are supported on all modern ESP32 but probably lacking on ESP32 classic, there we would need to do it in post-processing i.e. when writing the sample buffer
+ - need to add a "buffer full" callback?
+ - add namespace?
  */
 
-#include "wled_adcmanager.h"
+//#include "wled_adcmanager.h"
+#include "wled.h"
 
+//namespace wled {
 // prevent macro recursion of arduino overrides
 #undef analogRead
 #if defined(ARDUINO_ARCH_ESP32)
@@ -12,6 +25,8 @@
 #endif
 
 #ifdef ARDUINO_ARCH_ESP32
+
+#define DMA_BLOCKSIZE 128 // DMA buffer block size, IDF driver uses 5 blocks under the hood, there is an ISR call each time a block finishes so dont make it too small
 
 #include <string.h>
 
@@ -44,7 +59,16 @@ WLEDAdcManager& WLEDAdcManager::instance() {
 }
 
 WLEDAdcManager::WLEDAdcManager()
-  : _pin(0xFF), _channel(ADC_CHANNEL_0), _sampleRate(0), _samplesPerFrame(0), _handle(nullptr), _running(false), _cache(nullptr), _cacheSize(0), _cacheCount(0), _cali(nullptr) {
+  : _pin(0xFF)
+  , _channel(ADC_CHANNEL_0)
+  , _sampleRate(0)
+  , _samplesPerFrame(0)
+  , _handle(nullptr)
+  , _running(false)
+  , _cache(nullptr)
+  , _cacheSize(0)
+  , _cacheCount(0)
+  , _cali(nullptr) {
   _mutex = xSemaphoreCreateMutex();
 }
 
@@ -58,7 +82,7 @@ WLEDAdcManager::~WLEDAdcManager() {
 
 bool WLEDAdcManager::begin(uint8_t pin, uint32_t sampleRateHz, uint16_t samplesPerFrame) {
   xSemaphoreTake(_mutex, portMAX_DELAY);
-  _destroyHandle();
+  _endContinuousADC();
   _cacheCount = 0;
 
   if (!_pinToChannel(pin, &_channel)) {
@@ -73,29 +97,53 @@ bool WLEDAdcManager::begin(uint8_t pin, uint32_t sampleRateHz, uint16_t samplesP
   _cacheSize = samplesPerFrame;
   _cache = (int16_t*)calloc(_cacheSize, sizeof(int16_t));
 
-  bool ok = _createHandle();
+  bool ok = _initContinuousADC();
   xSemaphoreGive(_mutex);
   return ok;
 }
 
 void WLEDAdcManager::end() {
   xSemaphoreTake(_mutex, portMAX_DELAY);
-  _destroyHandle();
+  _endContinuousADC();
   _cacheCount = 0;
   if (_cache) { free(_cache); _cache = nullptr; _cacheSize = 0; }
   _pin = 0xFF;
   xSemaphoreGive(_mutex);
 }
 
-bool WLEDAdcManager::_createHandle() {
+/*
+// buffer overflow callback, we need to watch this as permanent overflow causes stalls in combination with wifi -> it does not it was an IDF bug
+volatile bool _overflow = false;
+static bool IRAM_ATTR __attribute__((noinline)) _onPoolOvf(adc_continuous_handle_t handle,
+                                  const adc_continuous_evt_data_t* edata,
+                                  void* user_data) {
+  //auto* mgr = static_cast<WLEDAdcManager*>(user_data);
+  _overflow = true;   // one word write, ISR-safe, no locks, no copying
+  return false;            // nothing to wake
+}
+
+void WLEDAdcManager::checkADC() {
+  if (_running && _overflow) {
+    adc_continuous_stop(_handle);
+    adc_continuous_flush_pool(_handle); // flush remaining data, we want fresh samples
+    adc_continuous_start(_handle);
+    _overflow = false;
+  }
+}
+*/
+bool WLEDAdcManager::_initContinuousADC() {
   if (_handle) return true;
   size_t frameBytes = (size_t)_samplesPerFrame * sizeof(adc_digi_output_data_t);
   adc_continuous_handle_cfg_t hcfg = {
-    .max_store_buf_size = frameBytes * 4,
-    .conv_frame_size    = frameBytes,
-    .flags = { .flush_pool = true },
+    .max_store_buf_size = frameBytes * 1, // hold two frames in buffer, caller needs to drain it fast enough to avoid data loss
+    .conv_frame_size    = DMA_BLOCKSIZE, // use fixed DMA buffer size of 256 bytes (ADC driver creates 5 DMA descriptors with one buffer each, at 20kHz this means an interrupt every 1.4ms
+    .flags = { .flush_pool = false }, // do not flush the store buffer on overrun but discard new samples (true means discard oldest, is much slower and can cause issues, do not set true)
   };
   if (adc_continuous_new_handle(&hcfg, &_handle) != ESP_OK) return false;
+
+  // register buffer overflow callback (sets flag, main loop needs to call checkADC() to clear overflow - this is to prevent wifi stalling due to a now fixed IDF bug causing a lockup)
+  //adc_continuous_evt_cbs_t cbs = { .on_conv_done = nullptr, .on_pool_ovf = _onPoolOvf };
+  //adc_continuous_register_event_callbacks(_handle, &cbs, this);
 
   adc_digi_pattern_config_t pat = {
     .atten     = ADC_ATTEN_DB_12,
@@ -112,14 +160,14 @@ bool WLEDAdcManager::_createHandle() {
   };
   if (adc_continuous_config(_handle, &cfg) != ESP_OK ||
     adc_continuous_start(_handle) != ESP_OK) {
-    _destroyHandle();
+    _endContinuousADC();
     return false;
   }
   _running = true;
   return true;
 }
 
-void WLEDAdcManager::_destroyHandle() {
+void WLEDAdcManager::_endContinuousADC() {
   if (_handle) {
     adc_continuous_stop(_handle);
     adc_continuous_deinit(_handle);
@@ -130,25 +178,27 @@ void WLEDAdcManager::_destroyHandle() {
 
 void WLEDAdcManager::_drainToCache() {
   if (!_handle || !_cache) return;
-  adc_continuous_stop(_handle);
-  adc_digi_output_data_t temp[64];
+  adc_digi_output_data_t temp[32]; // size of data packets to request, 32 samples at 22kHz is 1.5ms, leftover samples are lost
   _cacheCount = 0;
   while (_cacheCount < _cacheSize) {
     uint32_t n = 0;
+    // read what is available in the buffer in chunks (no timeout means do not wait for any additional samples)
     if (adc_continuous_read(_handle, (uint8_t*)temp, sizeof(temp), &n, 0) != ESP_OK || n == 0) break;
     uint16_t cnt = n / sizeof(adc_digi_output_data_t);
     for (uint16_t i = 0; i < cnt && _cacheCount < _cacheSize; i++) {
-      _cache[_cacheCount++] = (int16_t)((int)(temp[i].WLED_ADC_OUT_TYPE.data) - 2048);
+      _cache[_cacheCount++] = (int16_t)(temp[i].WLED_ADC_OUT_TYPE.data);
     }
   }
+  adc_continuous_stop(_handle); // stop after reading (if stopped before, no samples are being read)
 }
 
+// read samples acquired in continuous mode. They are written as 12bit unsigned values into the passed buffer
 bool WLEDAdcManager::readSamples(int16_t* buffer, uint16_t numSamples, uint32_t timeoutMs) {
-  if (!buffer || !numSamples) return false;
+  if (!_running || !buffer || !numSamples) return false;
   xSemaphoreTake(_mutex, portMAX_DELAY);
 
   uint16_t out = 0;
-
+  // note: DMA uses SOC_ADC_DIGI_MAX_BITWIDTH which is 12bits on all checked units TODO: should make sure and handle this to future proof it
   if (_cacheCount) {
     uint16_t copy = _cacheCount < numSamples ? _cacheCount : numSamples;
     memcpy(buffer, _cache, copy * sizeof(int16_t));
@@ -167,46 +217,63 @@ bool WLEDAdcManager::readSamples(int16_t* buffer, uint16_t numSamples, uint32_t 
     xSemaphoreGive(_mutex);
     return false;
   }
-
-  adc_continuous_start(_handle);
-  adc_digi_output_data_t temp[64];
+  const int tmpBfrSize = 128; // use buffer on stack to read samples from the driver
+  adc_digi_output_data_t temp[tmpBfrSize];
 
   while (out < numSamples) {
     uint32_t n = 0;
-    uint16_t want = (numSamples - out) < 64 ? (numSamples - out) : 64;
+    uint16_t want = (numSamples - out) < tmpBfrSize ? (numSamples - out) : tmpBfrSize;
     size_t wantBytes = want * sizeof(adc_digi_output_data_t);
-
-    if (adc_continuous_read(_handle, (uint8_t*)temp, wantBytes, &n, pdMS_TO_TICKS(timeoutMs)) != ESP_OK || n == 0) {
-      adc_continuous_stop(_handle);
+    esp_err_t err = adc_continuous_read(_handle, (uint8_t*)temp, wantBytes, &n, pdMS_TO_TICKS(timeoutMs));
+    // TODO:  add the possibility to "read what is available" and return the number of samples read?
+    if (err != ESP_OK || n == 0) {
+      // something went wrong, re-init the continuous transfer
+      DEBUG_PRINTF_P(PSTR("ADC read error %d, got n=%d restarting"),err,n);
+      _endContinuousADC();  // tear it down
+      _initContinuousADC(); // re-init and start sampling
       xSemaphoreGive(_mutex);
       return false;
     }
+    // copy the data into 16bit buffer
     uint16_t got = n / sizeof(adc_digi_output_data_t);
     for (uint16_t i = 0; i < got; i++) {
-      buffer[out++] = (int16_t)((int)(temp[i].WLED_ADC_OUT_TYPE.data) - 2048);
+      buffer[out++] = (int16_t)(temp[i].WLED_ADC_OUT_TYPE.data); // TODO: need to check bit width? see not above
     }
   }
 
-  adc_continuous_stop(_handle);
+  //adc_continuous_flush_pool(_handle); // flush remaining data -> no need, just let the samples accumulate
+
   xSemaphoreGive(_mutex);
   return true;
 }
 
+// a one-shot read takes about 0.8-2.5ms if continuous reading is active, 0.2ms otherwise
 bool WLEDAdcManager::_oneshotRead(adc_channel_t ch, int* outRaw) {
   adc_oneshot_unit_handle_t h;
-  adc_oneshot_unit_init_cfg_t icfg = { .unit_id = ADC_UNIT_1, .ulp_mode = ADC_ULP_MODE_DISABLE };
+  adc_oneshot_unit_init_cfg_t icfg = {
+    .unit_id = ADC_UNIT_1,
+    .ulp_mode = ADC_ULP_MODE_DISABLE };
   if (adc_oneshot_new_unit(&icfg, &h) != ESP_OK) return false;
 
-  adc_oneshot_chan_cfg_t ccfg = { .atten = ADC_ATTEN_DB_12, .bitwidth = ADC_BITWIDTH_12 };
+  adc_oneshot_chan_cfg_t ccfg = {
+    .atten = ADC_ATTEN_DB_12,
+    .bitwidth = ADC_BITWIDTH_12 };
   adc_oneshot_config_channel(h, ch, &ccfg);
 
   bool ok = (adc_oneshot_read(h, ch, outRaw) == ESP_OK);
   adc_oneshot_del_unit(h);
+  if (ok) {
+    // The raw result is in SOC_ADC_RTC_MAX_BITWIDTH bits, independent of .bitwidth set above, we use 12bit in WLED
+    #if (SOC_ADC_RTC_MAX_BITWIDTH > 12)
+      *outRaw >>= (SOC_ADC_RTC_MAX_BITWIDTH - 12);
+    #elif (SOC_ADC_RTC_MAX_BITWIDTH < 12)
+      *outRaw <<= (12 - SOC_ADC_RTC_MAX_BITWIDTH);
+    #endif
+  }
   return ok;
 }
 
 int WLEDAdcManager::analogRead(uint8_t pin) {
-  Serial.println("ADCread");
   int raw = 0;
   adc_channel_t ch;
   if (!_pinToChannel(pin, &ch)) return 0;
@@ -214,9 +281,9 @@ int WLEDAdcManager::analogRead(uint8_t pin) {
   xSemaphoreTake(_mutex, portMAX_DELAY);
   if (_running) {
     _drainToCache();
-    _destroyHandle();
+    _endContinuousADC();
     _oneshotRead(ch, &raw);
-    _createHandle();
+    _initContinuousADC(); // re-init and start sampling again
   } else {
     _oneshotRead(ch, &raw);
   }
@@ -237,7 +304,13 @@ bool WLEDAdcManager::_initCali() {
   return false;
 }
 
+#if SOC_ADC_DIG_IIR_FILTER_SUPPORTED
+#endif
+
 int WLEDAdcManager::analogReadMilliVolts(uint8_t pin) {
+  int result_mv = 0;
+  adc_channel_t ch;
+  if (!_pinToChannel(pin, &ch)) return 0;
   int raw = analogRead(pin);
   if (!_cali && !_initCali()) return (raw * 3300) / 4095;
 
@@ -250,20 +323,21 @@ int WLEDAdcManager::analogReadMilliVolts(uint8_t pin) {
 //  C wrappers
 extern "C" {
 
-int wled_adc_analog_read(uint8_t pin) {
-#ifdef ARDUINO_ARCH_ESP32
-  return WLEDAdcManager::instance().analogRead(pin);
-#else
-  eturn analogRead(pin);
-#endif
-}
+  int wled_adc_analog_read(uint8_t pin) {
+  #ifdef ARDUINO_ARCH_ESP32
+    return WLEDAdcManager::instance().analogRead(pin);
+  #else
+    return analogRead(pin);
+  #endif
+  }
 
-int wled_adc_analog_read_mv(uint8_t pin) {
-#ifdef ARDUINO_ARCH_ESP32
-  return WLEDAdcManager::instance().analogReadMilliVolts(pin);
-#else
-  eturn (int)(analogRead(pin) / 1023.0f);
-#endif
-}
+  int wled_adc_analog_read_mv(uint8_t pin) {
+  #ifdef ARDUINO_ARCH_ESP32
+    return WLEDAdcManager::instance().analogReadMilliVolts(pin);
+  #else
+    return (int)(analogRead(pin) / 1023.0f);
+  #endif
+  }
 
 }
+//} // namespace wled
