779dc3466a549f20e5cc0e75b97aa935ef2d69d2
Tim Leonard <timothyl@spotify.com>
2026-08-17 19:07:50 -0400

    Address paired RGBW bus review feedback

--- wled00/const.h
+++ wled00/const.h
@@ -356,7 +356,7 @@ static_assert(WLED_MAX_BUSSES <= 32, "WLED_MAX_BUSSES exceeds hard limit");
 #define TYPE_WS2805              32            //RGB + WW + CW
 #define TYPE_TM1914              33            //RGB
 #define TYPE_SM16825             34            //RGB + WW + CW
-#define TYPE_WS2812_RGBW_PAIR    35            //paired WS281x pixels: WW, then RGB
+#define TYPE_WS2812_RGBW_PAIR    35            // paired WS281x pixels: WW, then RGB
 #define TYPE_DIGITAL_MAX         39            // last usable digital type
 //"Analog" types (40-47)
 #define TYPE_ONOFF               40            //binary output (relays etc.; NOT PWM)
